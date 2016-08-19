6d2d4fe3f46173e3fe742745a4bdb7fc571d2803
alexv7 <alexvarjabedian7@gmail.com>
2016-08-19 09:44:59 -0400

    glyphicons working, also added stylesheets and js files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,12 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf .woff2 )
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
